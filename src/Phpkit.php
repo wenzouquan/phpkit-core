03a9b69155a13e44b9d3918be4e785466b9382ae
wenzouquan <359945126@qq.com>
2017-08-17 15:14:44 +0800

    12

--- src/Phpkit.php
+++ src/Phpkit.php
@@ -220,11 +220,7 @@ class Phpkit {
 					return $config;
 				};
 			}
-			//设置servicesApi
-			if (!empty($config['servicesApi'])) {
-				$rpcClient = new \phpkit\thriftrpc\Client();
-				$rpcClient->registerDefinition($config['servicesApi']);
-			}
+
 			self::$di = $di;
 			//执行action
 			//call_user_func_array(array($controller, $actionName . "Action"), $params);
