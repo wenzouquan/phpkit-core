8e68c135317434102ed27afc019ba2c2c5f142bf
wenzouquan <359945126@qq.com>
2017-08-31 17:41:58 +0800

    12

--- src/Phpkit.php
+++ src/Phpkit.php
@@ -75,17 +75,17 @@ class Phpkit {
 		echo $application->handle()->getContent();
 	}
 
-	public function setXdebugSession(){
-        if(isset($_GET['XDEBUG_SESSION_START'])){
-            $xdebugSession = $_GET['XDEBUG_SESSION_START'];
-            \apc_store("XDEBUG_SESSION_START",$xdebugSession);
-        }
-    }
+//	public function setXdebugSession(){
+//        if(isset($_GET['XDEBUG_SESSION_START'])){
+//            $xdebugSession = $_GET['XDEBUG_SESSION_START'];
+//            \apc_store("XDEBUG_SESSION_START",$xdebugSession);
+//        }
+//    }
 
 	public function init($config = array()) {
 		try {
 			error_reporting(E_ALL ^ E_NOTICE);
-            $this->setXdebugSession();
+            //$this->setXdebugSession();
 			if (empty($config['date_default_timezone_set'])) {
 				date_default_timezone_set('PRC'); //设置为北京时间
 			} else {
